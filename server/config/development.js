0a179947dfac3374b768ac3e9d54ce077041eeab
VAN HAAREN Lucas <vanhaarenlucas.dev@gmail.com>
2019-12-29 22:40:20 +0100

    feat(server): add configuration
    Add 'config' npm module as dependency
    Add config files for each env

---
+++ server/config/development.js
@@ -0,0 +1,13 @@
+module.exports = {
+    app: {
+        mode: "development",
+        logs: "dev",
+        port: 3333
+    },
+    db: {
+        name: "kasaz",
+        host: "localhost",
+        port: 27017,
+        logs: true
+    }
+};

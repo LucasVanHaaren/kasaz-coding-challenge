4c7c3e7a7836252f1cbc412b6dbaa1f38ee37000
VAN HAAREN Lucas <vanhaarenlucas.dev@gmail.com>
2019-12-30 16:16:38 +0100

    feat(client): add basic appBar and toolBar

---
+++ client/src/components/ToolBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-toolbar color="purple darken-2" dark>
+    <v-text-field
+      solo
+      light
+      background-color="white"
+      color="green"
+      flat
+      hide-details
+      placeholder="Search"
+      dense
+      clearable
+    />
+    <v-btn icon color="green">
+      <v-icon>fas fa-search</v-icon>
+    </v-btn>
+    <template v-if="$vuetify.breakpoint.mdAndUp">
+      <v-btn icon>
+        <v-icon>mdi-export-variant</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-delete-circle</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-plus-circle</v-icon>
+      </v-btn>
+    </template>
+    <template v-else>
+      <v-btn text>
+        Filtros
+        <v-icon color="green" right>fas fa-chevron-down</v-icon>
+        <!-- <v-icon color="green" right>fas fa-chevron-up</v-icon> -->
+      </v-btn>
+    </template>
+  </v-toolbar>
+</template>
+
+<script>
+
+export default {
+  name: 'ToolBar',
+  data: () => ({
+    // 
+  })
+};
+</script>

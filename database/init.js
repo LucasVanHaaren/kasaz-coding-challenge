2fc614f99fca699c886e65987f9ae2db70b2f87e
VAN HAAREN Lucas <vanhaarenlucas.dev@gmail.com>
2019-12-30 12:40:15 +0100

    boot(database): Add init db script
    Add sample data to the db at startup

---
+++ database/init.js
@@ -0,0 +1,35 @@
+db.apartments.save({
+    title: "Piso exterior y en buen estado con ascensor Sant Marti",
+    price: 280000,
+    sqm: 103,
+    bedroomsCount: 2,
+    bathroomsCount: 1
+})
+db.apartments.save({
+    title: "Piso exterior de diseno recien reformado en l'Eixample",
+    price: 479000,
+    sqm: 197,
+    bedroomsCount: 3,
+    bathroomsCount: 2
+})
+db.apartments.save({
+    title: "Amplio piso exterior y en buen estado en El Gotic",
+    price: 490000,
+    sqm: 115,
+    bedroomsCount: 3,
+    bathroomsCount: 2
+})
+db.apartments.save({
+    title: "Piso en venta en El Born (Santa Caterina)",
+    price: 238000,
+    sqm: 54,
+    bedroomsCount: 1,
+    bathroomsCount: 1
+})
+db.apartments.save({
+    title: "Piso Amueblado y reformado con detalle",
+    price: 475000,
+    sqm: 77,
+    bedroomsCount: 2,
+    bathroomsCount: 2
+})

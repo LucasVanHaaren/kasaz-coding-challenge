d830e08ef15413e3b7aded63207fbb59ac1dcdbc
VAN HAAREN Lucas <vanhaarenlucas.dev@gmail.com>
2019-12-30 16:16:38 +0100

    feat(client): add basic appBar and toolBar

---
+++ client/src/components/AppBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar app flat>
+    <v-app-bar-nav-icon color="green"></v-app-bar-nav-icon>
+    <div class="d-flex align-center">
+      <v-img
+        :src="require('../assets/logo.svg')"
+        class="shrink mr-2"
+        contain
+        transition="scale-transition"
+        width="180"
+      ></v-img>
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+
+export default {
+  name: 'AppBar',
+  data: () => ({
+    // 
+  })
+};
+</script>

b01d20db07f49eee2856e3c938b39f5cc0eec852
VAN HAAREN Lucas <vanhaarenlucas.dev@gmail.com>
2020-01-02 15:42:23 +0100

    feat(client): add Home view

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ToolBar
+      :showFilters="showFilterTab"
+      @filtersClick="handleFiltersClick"
+      @searchBarClick="handleSearchBarClick"
+    />
+    <FilterTab v-if="showFilterTab" />
+    <template v-for="(apartment, index) in apartments">
+      <Apartment v-bind="apartment" :key="index" class="apartment" />
+    </template>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ToolBar from '@/components/ToolBar.vue'
+import Apartment from '@/components/Apartment.vue'
+import FilterTab from '@/components/FilterTab.vue'
 
 export default {
-  name: 'home',
+  name: 'Home',
   components: {
-    HelloWorld
+    ToolBar,
+    Apartment,
+    FilterTab
+  },
+  data: () => ({
+    showFilterTab: false,
+    apartments: [
+      {
+        title: "Piso exterior y en buen estado con ascensor Sant Marti",
+        price: 280000,
+        sqm: 103,
+        bedroomsCount: 4,
+        bathroomsCount: 2,
+        thumbnail: "/pictures/sample1.jpg"
+      },
+      {
+        title: "Piso exterior y pres dos Sant Marti",
+        price: 120000,
+        sqm: 97,
+        bedroomsCount: 2,
+        bathroomsCount: 1,
+        thumbnail: "/pictures/sample2.jpg"
+      }
+    ]
+  }),
+  methods: {
+    handleSearchBarClick: function(data) {
+      // TODO
+      console.log(`handleSearchBarClick: ${data}`);
+    },
+    handleFiltersClick: function() {
+      this.showFilterTab = !this.showFilterTab;
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.apartment {
+  margin-top: 0.5em;
+}
+</style>

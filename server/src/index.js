29158ae895f9168b885b0b5d4d65fe9a5d10c7c0
VAN HAAREN Lucas <vanhaarenlucas.dev@gmail.com>
2020-01-03 22:35:22 +0100

    feat(server): add CORS policy

--- server/src/index.js
+++ server/src/index.js
@@ -2,9 +2,13 @@ const app = require("express")();
 const morgan = require("morgan");
 const config = require("config");
 const startup = require("./utils/startup");
+const cors = require("cors");
 
 const apartments = require("./routes/apartments");
 
+app.use(cors({
+    origin: "*"
+}))
 app.use("/apartments", apartments);
 
 app.listen(config.get("app.port"), startup(config.get("app"), config.get("db")));

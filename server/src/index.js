334b55b7c2b259a60599a86b327be09592ee9fe9
VAN HAAREN Lucas <vanhaarenlucas.dev@gmail.com>
2020-01-04 23:53:07 +0100

    refactor(server): add body-parser and logger

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,5 @@
+const config = require("config");
 const app = require("./app");
+const startup = require("./utils/startup");
 
 app.listen(config.get("app.port"), startup(config.get("app"), config.get("db")));

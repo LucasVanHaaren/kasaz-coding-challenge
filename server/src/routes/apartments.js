c815b50c085ef9fc8b06183b46efc6d205c10b84
VAN HAAREN Lucas <vanhaarenlucas.dev@gmail.com>
2020-01-04 14:35:11 +0100

    feat(server): add server-side filters
    Add query filters on '/apartments' endpoint

--- server/src/routes/apartments.js
+++ server/src/routes/apartments.js
@@ -3,13 +3,35 @@ const apartment = require("../models/apartment");
 
 module.exports = (() => {
     route.get("/", (req, res, next) => {
-        apartment.find((err, apartments) => {
+        apartment.find(queryParser(req.query),(err, apartments) => {
             if(!err) {
                 res.status(200).send(apartments);
             } else {
                 res.status(500).send(err);
             }
         });
-    })
+    });
     return route;
 })();
+
+function queryParser(query) {
+    let filter = {};
+    if(query) {
+        if(query.title) {
+            filter.title = { $regex: query.title, $options: 'i'}
+        }
+        if(query.minPrice && query.maxPrice) {
+            filter.price = { $gte: query.minPrice, $lte: query.maxPrice }
+        }
+        if(query.minSqm && query.maxSqm) {
+            filter.sqm = { $gte: query.minSqm, $lte: query.maxSqm }
+        }
+        if(query.minBedroomsCount) {
+            filter.bedroomsCount = { $gte: query.minBedroomsCount }
+        }
+        if(query.minBathroomsCount) {
+            filter.bathroomsCount = { $gte: query.minBathroomsCount }
+        }
+    }
+    return filter;
+}

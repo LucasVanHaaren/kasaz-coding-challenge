62e517364b3b951e55b88db24b2889dc80e41eef
VAN HAAREN Lucas <vanhaarenlucas.dev@gmail.com>
2020-01-04 23:53:07 +0100

    refactor(server): add body-parser and logger

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,16 @@
 const app = require("express")();
 const morgan = require("morgan");
-const config = require("config");
-const startup = require("./utils/startup");
+const config = require("config").get("app");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const apartments = require("./routes/apartments");
 
 app.use(cors({
     origin: "*"
 }))
+app.use(bodyParser.json());
+app.use(morgan(config.logs));
 app.use("/apartments", apartments);
 
 module.exports = app;

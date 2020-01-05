35fd439d36f75da5b9f89388d9b378b86da0e573
VAN HAAREN Lucas <vanhaarenlucas.dev@gmail.com>
2020-01-05 23:21:09 +0100

    perf(database): fetch cropped images
    Crop init apartments thumbnails

--- database/init.js
+++ database/init.js
@@ -4,7 +4,7 @@ db.apartments.save({
     sqm: 103,
     bedroomsCount: 2,
     bathroomsCount: 1,
-    thumbnail: "https://images.unsplash.com/photo-1541123603104-512919d6a96c"
+    thumbnail: "https://images.unsplash.com/photo-1541123603104-512919d6a96c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=50"
 })
 db.apartments.save({
     title: "Piso exterior de diseno recien reformado en l'Eixample",
@@ -12,7 +12,7 @@ db.apartments.save({
     sqm: 197,
     bedroomsCount: 3,
     bathroomsCount: 2,
-    thumbnail: "https://images.unsplash.com/photo-1567767292278-a4f21aa2d36e"
+    thumbnail: "https://images.unsplash.com/photo-1567767292278-a4f21aa2d36e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=50"
 })
 db.apartments.save({
     title: "Amplio piso exterior y en buen estado en El Gotic",
@@ -20,7 +20,7 @@ db.apartments.save({
     sqm: 115,
     bedroomsCount: 3,
     bathroomsCount: 2,
-    thumbnail: "https://images.unsplash.com/photo-1550581190-9c1c48d21d6c"
+    thumbnail: "https://images.unsplash.com/photo-1550581190-9c1c48d21d6c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=50"
 })
 db.apartments.save({
     title: "Piso en venta en El Born (Santa Caterina)",
@@ -28,7 +28,7 @@ db.apartments.save({
     sqm: 54,
     bedroomsCount: 1,
     bathroomsCount: 1,
-    thumbnail: "https://images.unsplash.com/photo-1556228453-efd6c1ff04f6"
+    thumbnail: "https://images.unsplash.com/photo-1556228453-efd6c1ff04f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=50"
 })
 db.apartments.save({
     title: "Piso Amueblado y reformado con detalle",
@@ -36,7 +36,7 @@ db.apartments.save({
     sqm: 77,
     bedroomsCount: 2,
     bathroomsCount: 2,
-    thumbnail: "https://images.unsplash.com/photo-1560448204-603b3fc33ddc"
+    thumbnail: "https://images.unsplash.com/photo-1560448204-603b3fc33ddc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=50"
 })
 db.apartments.save({
     title: "Propiedad única por su composición y ubicación",
@@ -44,7 +44,7 @@ db.apartments.save({
     sqm: 139,
     bedroomsCount: 4,
     bathroomsCount: 2,
-    thumbnail: "https://images.unsplash.com/photo-1542889601-399c4f3a8402"
+    thumbnail: "https://images.unsplash.com/photo-1542889601-399c4f3a8402?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=50"
 })
 db.apartments.save({
     title: "Encantador piso junto al Arc de Triomf",
@@ -52,7 +52,7 @@ db.apartments.save({
     sqm: 68,
     bedroomsCount: 3,
     bathroomsCount: 2,
-    thumbnail: "https://images.unsplash.com/photo-1539693010221-cd218dfe6565"
+    thumbnail: "https://images.unsplash.com/photo-1539693010221-cd218dfe6565?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=50"
 })
 db.apartments.save({
     title: "Reformado piso de lujo",
@@ -60,7 +60,7 @@ db.apartments.save({
     sqm: 100,
     bedroomsCount: 2,
     bathroomsCount: 2,
-    thumbnail: "https://images.unsplash.com/photo-1503174971373-b1f69850bded"
+    thumbnail: "https://images.unsplash.com/photo-1503174971373-b1f69850bded?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=50"
 })
 db.apartments.save({
     title: "Piso de obra nueva en venta en Vila de Gràcia",
@@ -68,7 +68,7 @@ db.apartments.save({
     sqm: 103,
     bedroomsCount: 2,
     bathroomsCount: 1,
-    thumbnail: "https://images.unsplash.com/photo-1546540749-1642fed507fc"
+    thumbnail: "https://images.unsplash.com/photo-1546540749-1642fed507fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=50"
 })
 db.apartments.save({
     title: "Piso reformado con terraza y ascensor en Eixample",
@@ -76,5 +76,5 @@ db.apartments.save({
     sqm: 183,
     bedroomsCount: 3,
     bathroomsCount: 3,
-    thumbnail: "https://images.unsplash.com/photo-1533779283484-8ad4940aa3a8"
+    thumbnail: "https://images.unsplash.com/photo-1533779283484-8ad4940aa3a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=50"
 })
